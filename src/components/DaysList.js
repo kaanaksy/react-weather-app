dc29c4a25e8b85521883dd6d8e1ed3f7a9aa425a
Kaan Aksoy <kaanaksoya@gmail.com>
2022-12-30 12:30:06 +0300

    yeni api denendi

--- src/components/DaysList.js
+++ src/components/DaysList.js
@@ -6,13 +6,22 @@ import { useWeather } from "../context/WeatherContext"
 function DaysList() {
   const { days, weather, setWeather, city } = useWeather();
 
+  // useEffect(() => {
+  //   const API = "cef6a51450db9bf0ce10805f7a7da2ed";
+  //   const getData = async () => {
+  //     const URL = `https://api.openweathermap.org/data/3.0/onecall?lat=${city.latitude}&lon=${city.longitude}&exclude={part}&appid=${API}`;
+  //     await axios(URL).then(res => setWeather(res.data))
+  //   };
+  //   getData();
+  // }, [city, setWeather]);
+
   useEffect(() => {
-    const API = "cef6a51450db9bf0ce10805f7a7da2ed";
+    const API = "ff599af0562049b497aad3431c3c32a8";
     const getData = async () => {
-      const URL = `https://api.openweathermap.org/data/3.0/onecall?lat=${city.latitude}&lon=${city.longitude}&exclude={part}&appid=${API}`;
+      const URL = `https://api.weatherbit.io/v2.0/current?lat=${city.latitude}&lon=${city.longitude}&include=minutely&marine=t&lang=tr&key=${API}`;
       await axios(URL).then(res => setWeather(res.data))
-      getData();
     };
+    getData();
   }, [city, setWeather]);
 
   return (
